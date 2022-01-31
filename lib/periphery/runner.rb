88c54a5a5b7ba328953a39e1e5a2c6e7c0c1edc0
Ryosuke Ito <rito.0305@gmail.com>
2022-02-01 05:54:45 +0900

    Use lazy for performance

--- lib/periphery/runner.rb
+++ lib/periphery/runner.rb
@@ -22,10 +22,12 @@ module Periphery
 
     def scan_arguments(options)
       options.
+        lazy.
         select { |_key, value| value }.
         map { |key, value| value.kind_of?(TrueClass) ? [key, nil] : [key, value] }.
         map { |key, value| value.kind_of?(Array) ? [key, value.join(",")] : [key, value] }.
         map { |key, value| ["--#{key.to_s.tr('_', '-')}", value] }.
+        force.
         flatten.
         compact
     end
