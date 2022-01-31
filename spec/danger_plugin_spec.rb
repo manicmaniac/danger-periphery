606fcab9a55cf15d0291e42262a1c85d2d258b95
Ryosuke Ito <rito.0305@gmail.com>
2022-02-01 06:11:20 +0900

    Get specs out of module scope

--- spec/danger_plugin_spec.rb
+++ spec/danger_plugin_spec.rb
@@ -2,86 +2,84 @@
 
 require "danger_helper"
 
-module Danger
-  describe Danger::DangerPeriphery do
-    it "should be a plugin" do
-      expect(Danger::DangerPeriphery.new(nil)).to be_a Danger::Plugin
+describe Danger::DangerPeriphery do
+  it "should be a plugin" do
+    expect(Danger::DangerPeriphery.new(nil)).to be_a Danger::Plugin
+  end
+
+  context "with Dangerfile" do
+    let(:dangerfile) { testing_dangerfile }
+    let(:periphery) { dangerfile.periphery }
+
+    before do
+      periphery.binary_path = binary("periphery")
+      json = File.read("#{File.dirname(__FILE__)}/support/fixtures/github_pr.json") # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
+      allow(periphery.github).to receive(:pr_json).and_return(json)
+      allow(Pathname).to receive(:getwd).and_return fixtures_path
     end
 
-    context "with Dangerfile" do
-      let(:dangerfile) { testing_dangerfile }
-      let(:periphery) { dangerfile.periphery }
+    context "when periphery is not installed" do
+      before { periphery.binary_path = "not_installed" }
 
+      it "fails with error" do
+        expect { periphery.scan }.to raise_error Errno::ENOENT
+      end
+    end
+
+    context "when .swift files not in diff" do
       before do
-        periphery.binary_path = binary("periphery")
-        json = File.read("#{File.dirname(__FILE__)}/support/fixtures/github_pr.json") # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
-        allow(periphery.github).to receive(:pr_json).and_return(json)
-        allow(Pathname).to receive(:getwd).and_return fixtures_path
+        allow(periphery.git).to receive(:renamed_files).and_return []
+        allow(periphery.git).to receive(:modified_files).and_return []
+        allow(periphery.git).to receive(:deleted_files).and_return []
+        allow(periphery.git).to receive(:added_files).and_return []
       end
 
-      context "when periphery is not installed" do
-        before { periphery.binary_path = "not_installed" }
+      it "reports nothing" do
+        periphery.scan(
+          project: fixture("test.xcodeproj"),
+          targets: "test",
+          schemes: "test"
+        )
 
-        it "fails with error" do
-          expect { periphery.scan }.to raise_error Errno::ENOENT
-        end
+        expect(dangerfile.status_report[:warnings]).to be_empty
+      end
+    end
+
+    context "when .swift files were added" do
+      before do
+        allow(periphery.git).to receive(:renamed_files).and_return []
+        allow(periphery.git).to receive(:modified_files).and_return []
+        allow(periphery.git).to receive(:deleted_files).and_return []
+        allow(periphery.git).to receive(:added_files).and_return ["test/main.swift"]
       end
 
-      context "when .swift files not in diff" do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return []
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return []
-        end
+      it "reports unused code" do
+        periphery.scan(
+          project: fixture("test.xcodeproj"),
+          targets: "test",
+          schemes: "test"
+        )
 
-        it "reports nothing" do
-          periphery.scan(
-            project: fixture("test.xcodeproj"),
-            targets: "test",
-            schemes: "test"
-          )
+        expect(dangerfile.status_report[:warnings]).to eq(["Enum 'UnusedEnum' is unused"])
+      end
+    end
 
-          expect(dangerfile.status_report[:warnings]).to be_empty
-        end
+    context "when .swift files were modified" do
+      before do
+        allow(periphery.git).to receive(:renamed_files).and_return []
+        allow(periphery.git).to receive(:modified_files).and_return ["test/main.swift"]
+        allow(periphery.git).to receive(:deleted_files).and_return []
+        allow(periphery.git).to receive(:added_files).and_return []
       end
 
-      context "when .swift files were added" do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return []
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return ["test/main.swift"]
-        end
+      it "reports unused code" do
+        periphery.scan(
+          project: fixture("test.xcodeproj"),
+          targets: "test",
+          schemes: "test"
+        )
 
-        it "reports unused code" do
-          periphery.scan(
-            project: fixture("test.xcodeproj"),
-            targets: "test",
-            schemes: "test"
-          )
-
-          expect(dangerfile.status_report[:warnings]).to eq(["Enum 'UnusedEnum' is unused"])
-        end
-      end
-
-      context "when .swift files were modified" do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return ["test/main.swift"]
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return []
-        end
-
-        it "reports unused code" do
-          periphery.scan(
-            project: fixture("test.xcodeproj"),
-            targets: "test",
-            schemes: "test"
-          )
-
-          expect(dangerfile.status_report[:warnings]).to eq(["Enum 'UnusedEnum' is unused"])
-        end
+        expect(dangerfile.status_report[:warnings]).to eq(["Enum 'UnusedEnum' is unused"])
       end
     end
   end
