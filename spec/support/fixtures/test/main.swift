eebb39add10edcb8769f633ddd123892cc2e1bfa
Ryosuke Ito <rito.0305@gmail.com>
2022-02-01 12:58:53 +0900

    Cover all warning types in test fixture

--- spec/support/fixtures/test/main.swift
+++ spec/support/fixtures/test/main.swift
@@ -1,3 +1,23 @@
-enum UnusedEnum {
-    case unused
+protocol RedundantProtocol {
 }
+
+class SomeClass: RedundantProtocol {
+    enum SomeEnum {
+        case usedCase
+        case unusedCase
+    }
+
+    var unusedProperty = 0
+    private var assignOnlyProperty = 0
+
+
+    public func methodWithRedundantPublicAccessibility(_ unusedParameter: Int) {
+        assignOnlyProperty = 0
+        _ = SomeEnum.usedCase
+    }
+
+    func unusedMethod() {
+    }
+}
+
+SomeClass().methodWithRedundantPublicAccessibility(0)
