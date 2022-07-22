37d764eb082d065cfa41b8541cb19c3f1b7922ee
Ryosuke Ito <rito.0305@gmail.com>
2022-07-22 11:19:47 +0900

    Use mock executable for test

--- spec/danger_plugin_spec.rb
+++ spec/danger_plugin_spec.rb
@@ -20,7 +20,7 @@ describe Danger::DangerPeriphery do
     let(:periphery) { dangerfile.periphery }
 
     before do
-      periphery.binary_path = binary("periphery")
+      periphery.binary_path = fixture("mock-periphery")
       json = File.read("#{File.dirname(__FILE__)}/support/fixtures/github_pr.json") # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
       allow(periphery.github).to receive(:pr_json).and_return(json)
       allow(Pathname).to receive(:getwd).and_return fixtures_path
@@ -34,48 +34,52 @@ describe Danger::DangerPeriphery do
       end
     end
 
-    context "when .swift files not in diff" do
-      before do
-        allow(periphery.git).to receive(:renamed_files).and_return []
-        allow(periphery.git).to receive(:modified_files).and_return []
-        allow(periphery.git).to receive(:deleted_files).and_return []
-        allow(periphery.git).to receive(:added_files).and_return []
+    context "with a real periphery executable" do
+      before { periphery.binary_path = binary("periphery") }
+
+      context "when .swift files not in diff" do
+        before do
+          allow(periphery.git).to receive(:renamed_files).and_return []
+          allow(periphery.git).to receive(:modified_files).and_return []
+          allow(periphery.git).to receive(:deleted_files).and_return []
+          allow(periphery.git).to receive(:added_files).and_return []
+        end
+
+        it "reports nothing" do
+          periphery.scan(periphery_options)
+
+          expect(dangerfile.status_report[:warnings]).to be_empty
+        end
       end
 
-      it "reports nothing" do
-        periphery.scan(periphery_options)
+      context "when .swift files were added" do
+        before do
+          allow(periphery.git).to receive(:renamed_files).and_return []
+          allow(periphery.git).to receive(:modified_files).and_return []
+          allow(periphery.git).to receive(:deleted_files).and_return []
+          allow(periphery.git).to receive(:added_files).and_return ["test/main.swift"]
+        end
 
-        expect(dangerfile.status_report[:warnings]).to be_empty
-      end
-    end
+        it "reports unused code" do
+          periphery.scan(periphery_options)
 
-    context "when .swift files were added" do
-      before do
-        allow(periphery.git).to receive(:renamed_files).and_return []
-        allow(periphery.git).to receive(:modified_files).and_return []
-        allow(periphery.git).to receive(:deleted_files).and_return []
-        allow(periphery.git).to receive(:added_files).and_return ["test/main.swift"]
-      end
-
-      it "reports unused code" do
-        periphery.scan(periphery_options)
-
-        expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
-      end
-    end
-
-    context "when .swift files were modified" do
-      before do
-        allow(periphery.git).to receive(:renamed_files).and_return []
-        allow(periphery.git).to receive(:modified_files).and_return ["test/main.swift"]
-        allow(periphery.git).to receive(:deleted_files).and_return []
-        allow(periphery.git).to receive(:added_files).and_return []
+          expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
+        end
       end
 
-      it "reports unused code" do
-        periphery.scan(periphery_options)
+      context "when .swift files were modified" do
+        before do
+          allow(periphery.git).to receive(:renamed_files).and_return []
+          allow(periphery.git).to receive(:modified_files).and_return ["test/main.swift"]
+          allow(periphery.git).to receive(:deleted_files).and_return []
+          allow(periphery.git).to receive(:added_files).and_return []
+        end
 
-        expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
+        it "reports unused code" do
+          periphery.scan(periphery_options)
+
+          expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
+        end
       end
     end
 
