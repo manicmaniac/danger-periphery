b752bf173943c29c55b53ca4af195cd8bbd1cc13
Ryosuke Ito <rito.0305@gmail.com>
2022-07-26 10:28:39 +0900

    Reduce context nesting

--- spec/danger_plugin_spec.rb
+++ spec/danger_plugin_spec.rb
@@ -3,6 +3,8 @@
 describe Danger::DangerPeriphery do
   include DangerPluginHelper
 
+  let(:dangerfile) { testing_dangerfile }
+  let(:periphery) { dangerfile.periphery }
   let(:periphery_options) do
     {
       project: fixture('test.xcodeproj'),
@@ -11,185 +13,179 @@ describe Danger::DangerPeriphery do
     }
   end
 
+  before do
+    periphery.binary_path = fixture('mock-periphery')
+    # example json: `curl -o github_pr.json https://api.github.com/repos/danger/danger-plugin-template/pulls/18
+    json = File.read(fixture('github_pr.json'))
+    allow(periphery.github).to receive(:pr_json).and_return json
+    allow(Pathname).to receive(:getwd).and_return fixtures_path
+  end
+
   it 'is a plugin' do
     expect(described_class.new(nil)).to be_a Danger::Plugin
   end
 
-  context 'with Dangerfile' do
-    let(:dangerfile) { testing_dangerfile }
-    let(:periphery) { dangerfile.periphery }
+  context 'when periphery is not installed' do
+    before { periphery.binary_path = 'not_installed' }
 
-    before do
-      periphery.binary_path = fixture('mock-periphery')
-      # example json: `curl -o github_pr.json https://api.github.com/repos/danger/danger-plugin-template/pulls/18
-      json = File.read(fixture('github_pr.json'))
-      allow(periphery.github).to receive(:pr_json).and_return json
-      allow(Pathname).to receive(:getwd).and_return fixtures_path
+    it 'fails with error' do
+      expect { periphery.scan }.to raise_error Errno::ENOENT
     end
+  end
 
-    context 'when periphery is not installed' do
-      before { periphery.binary_path = 'not_installed' }
+  context 'with a real periphery executable', :slow do
+    subject(:warnings) { dangerfile.status_report[:warnings] }
 
-      it 'fails with error' do
-        expect { periphery.scan }.to raise_error Errno::ENOENT
-      end
-    end
+    before { periphery.binary_path = binary('periphery') }
 
-    context 'with a real periphery executable', :slow do
-      before { periphery.binary_path = binary('periphery') }
-
-      context 'when .swift files not in diff' do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return []
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return []
-        end
-
-        it 'reports nothing' do
-          periphery.scan(periphery_options)
-
-          expect(dangerfile.status_report[:warnings]).to be_empty
-        end
-      end
-
-      context 'when .swift files were added' do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return []
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return ['test/main.swift']
-        end
-
-        it 'reports unused code' do
-          periphery.scan(periphery_options)
-
-          expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
-        end
-      end
-
-      context 'when .swift files were modified' do
-        before do
-          allow(periphery.git).to receive(:renamed_files).and_return []
-          allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
-          allow(periphery.git).to receive(:deleted_files).and_return []
-          allow(periphery.git).to receive(:added_files).and_return []
-        end
-
-        it 'reports unused code' do
-          periphery.scan(periphery_options)
-
-          expect(dangerfile.status_report[:warnings]).to include "Function 'unusedMethod()' is unused"
-        end
-      end
-    end
-
-    context 'with block' do
-      subject(:warnings) { dangerfile.status_report[:warnings] }
-
+    context 'when .swift files not in diff' do
       before do
         allow(periphery.git).to receive(:renamed_files).and_return []
-        allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
+        allow(periphery.git).to receive(:modified_files).and_return []
         allow(periphery.git).to receive(:deleted_files).and_return []
         allow(periphery.git).to receive(:added_files).and_return []
-        periphery.scan(periphery_options, &block)
-      end
-
-      context 'that returns nil' do
-        let(:block) { ->(violation) {} }
-
-        it 'filters out all warnings' do
-          expect(warnings).to be_empty
-        end
-      end
-
-      context 'that returns false' do
-        let(:block) { ->(_violation) { false } }
-
-        it 'filters out all warnings' do
-          expect(warnings).to be_empty
-        end
-      end
-
-      context 'that returns true' do
-        let(:block) { ->(_violation) { true } }
-
-        it 'reports warnings without filtering anything' do
-          expect(warnings).to include "Function 'unusedMethod()' is unused"
-        end
-      end
-
-      context 'that returns truthy value' do
-        let(:block) { ->(_violation) { 0 } }
-
-        it 'reports warnings without filtering anything' do
-          expect(warnings).to include "Function 'unusedMethod()' is unused"
-        end
-      end
-
-      context 'that modifies the given violation' do
-        let(:block) { ->(violation) { violation.message.gsub!(/Function/, 'Foobar') } }
-
-        it 'reports modified warnings' do
-          expect(warnings).to include "Foobar 'unusedMethod()' is unused"
-        end
-      end
-    end
-
-    describe '#postprocessor' do
-      subject(:warnings) { dangerfile.status_report[:warnings] }
-
-      before do
-        allow(periphery.git).to receive(:renamed_files).and_return []
-        allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
-        allow(periphery.git).to receive(:deleted_files).and_return []
-        allow(periphery.git).to receive(:added_files).and_return []
-        periphery.postprocessor = postprocessor
         periphery.scan(periphery_options)
       end
 
-      context 'when returns nil' do
-        let(:postprocessor) { ->(path, line, column, message) {} }
-
-        it 'does not report warnings' do
-          expect(warnings).to be_empty
-        end
-      end
-
-      context 'when returns false' do
-        let(:postprocessor) { ->(_path, _line, _column, _message) { false } }
-
-        it 'does not report warnings' do
-          expect(warnings).to be_empty
-        end
-      end
-
-      context 'when returns true' do
-        let(:postprocessor) { ->(_path, _line, _column, _message) { true } }
-
-        it 'reports warnings' do
-          expect(warnings).to include "Function 'unusedMethod()' is unused"
-        end
-      end
-
-      context 'when returns a modified array' do
-        let(:postprocessor) do
-          ->(path, line, column, message) { [path, line, column, message.gsub(/Function/, 'Foobar')] }
-        end
-
-        it 'reports modified warnings' do
-          expect(warnings).to include "Foobar 'unusedMethod()' is unused"
-        end
+      it 'reports nothing' do
+        expect(warnings).to be_empty
       end
     end
 
-    describe '#process_warnings' do
-      it 'sets postprocessor' do
-        periphery.process_warnings do |_path, _line, _column, _message|
-          nil
-        end
-        expect { periphery.process_warnings { |*_args| nil } }.to change(periphery, :postprocessor)
+    context 'when .swift files were added' do
+      before do
+        allow(periphery.git).to receive(:renamed_files).and_return []
+        allow(periphery.git).to receive(:modified_files).and_return []
+        allow(periphery.git).to receive(:deleted_files).and_return []
+        allow(periphery.git).to receive(:added_files).and_return ['test/main.swift']
+        periphery.scan(periphery_options)
       end
+
+      it 'reports unused code' do
+        expect(warnings).to include "Function 'unusedMethod()' is unused"
+      end
+    end
+
+    context 'when .swift files were modified' do
+      before do
+        allow(periphery.git).to receive(:renamed_files).and_return []
+        allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
+        allow(periphery.git).to receive(:deleted_files).and_return []
+        allow(periphery.git).to receive(:added_files).and_return []
+        periphery.scan(periphery_options)
+      end
+
+      it 'reports unused code' do
+        expect(warnings).to include "Function 'unusedMethod()' is unused"
+      end
+    end
+  end
+
+  context 'with block' do
+    subject(:warnings) { dangerfile.status_report[:warnings] }
+
+    before do
+      allow(periphery.git).to receive(:renamed_files).and_return []
+      allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
+      allow(periphery.git).to receive(:deleted_files).and_return []
+      allow(periphery.git).to receive(:added_files).and_return []
+      periphery.scan(periphery_options, &block)
+    end
+
+    context 'that returns nil' do
+      let(:block) { ->(violation) {} }
+
+      it 'filters out all warnings' do
+        expect(warnings).to be_empty
+      end
+    end
+
+    context 'that returns false' do
+      let(:block) { ->(_violation) { false } }
+
+      it 'filters out all warnings' do
+        expect(warnings).to be_empty
+      end
+    end
+
+    context 'that returns true' do
+      let(:block) { ->(_violation) { true } }
+
+      it 'reports warnings without filtering anything' do
+        expect(warnings).to include "Function 'unusedMethod()' is unused"
+      end
+    end
+
+    context 'that returns truthy value' do
+      let(:block) { ->(_violation) { 0 } }
+
+      it 'reports warnings without filtering anything' do
+        expect(warnings).to include "Function 'unusedMethod()' is unused"
+      end
+    end
+
+    context 'that modifies the given violation' do
+      let(:block) { ->(violation) { violation.message.gsub!(/Function/, 'Foobar') } }
+
+      it 'reports modified warnings' do
+        expect(warnings).to include "Foobar 'unusedMethod()' is unused"
+      end
+    end
+  end
+
+  describe '#postprocessor' do
+    subject(:warnings) { dangerfile.status_report[:warnings] }
+
+    before do
+      allow(periphery.git).to receive(:renamed_files).and_return []
+      allow(periphery.git).to receive(:modified_files).and_return ['test/main.swift']
+      allow(periphery.git).to receive(:deleted_files).and_return []
+      allow(periphery.git).to receive(:added_files).and_return []
+      periphery.postprocessor = postprocessor
+      periphery.scan(periphery_options)
+    end
+
+    context 'when returns nil' do
+      let(:postprocessor) { ->(path, line, column, message) {} }
+
+      it 'does not report warnings' do
+        expect(warnings).to be_empty
+      end
+    end
+
+    context 'when returns false' do
+      let(:postprocessor) { ->(_path, _line, _column, _message) { false } }
+
+      it 'does not report warnings' do
+        expect(warnings).to be_empty
+      end
+    end
+
+    context 'when returns true' do
+      let(:postprocessor) { ->(_path, _line, _column, _message) { true } }
+
+      it 'reports warnings' do
+        expect(warnings).to include "Function 'unusedMethod()' is unused"
+      end
+    end
+
+    context 'when returns a modified array' do
+      let(:postprocessor) do
+        ->(path, line, column, message) { [path, line, column, message.gsub(/Function/, 'Foobar')] }
+      end
+
+      it 'reports modified warnings' do
+        expect(warnings).to include "Foobar 'unusedMethod()' is unused"
+      end
+    end
+  end
+
+  describe '#process_warnings' do
+    it 'sets postprocessor' do
+      periphery.process_warnings do |_path, _line, _column, _message|
+        nil
+      end
+      expect { periphery.process_warnings { |*_args| nil } }.to change(periphery, :postprocessor)
     end
   end
 end
