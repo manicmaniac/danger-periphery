d270ae3757e0ed62ec011435d1c1fc064a8b4006
Ryosuke Ito <rito.0305@gmail.com>
2025-02-17 09:49:36 +0900

    Report coverage in lcov format for vscode-coverage-gutters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,17 @@ require 'danger'
 require 'pry'
 require 'rspec'
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.output_directory = 'coverage'
+  config.lcov_file_name = 'lcov.info'
+end
 SimpleCov.start do
   load_profile 'test_frameworks'
   enable_coverage :branch
+  formatter SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::LcovFormatter,
+                                                      SimpleCov::Formatter::HTMLFormatter])
 end
 require 'danger_plugin'
 require 'support/helpers'
